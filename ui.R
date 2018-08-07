469c7ea22dd922381b344adecddb0e4ec1ee748a
Kyle E <kenns@usgs.gov>
2018-08-07 13:25:15 -0600

    fix merge fail

--- ui.R
+++ ui.R
@@ -1,11 +1,11 @@
 # UI file for Shiny App phenoRemote
 # Initiate the UI
 ui = fluidPage(shinyjs::useShinyjs(), navbarPage("PhenoSynth-development phase", id="navbar",
-                          
+
                           tabPanel("Site explorer",
-    
+
                                    div(class="outer",
-                                       
+
                                        tags$head(# Include custom CSS
                                                  includeCSS("styles.css"),
                                                  includeScript("gomap.js")
@@ -31,33 +31,27 @@ ui = fluidPage(shinyjs::useShinyjs(), navbarPage("PhenoSynth-development phase",
                                       actionButton('analyzerMode', 'Enter Analyze Mode'),
                                       checkboxInput("drawROI", "See Field of View (FOV)", value = FALSE),
                                       sliderInput("azm", "Toggle FOV:", min = 0, max = 360, value = 0.0 , step = 5),
-<<<<<<< HEAD
-                                      checkboxInput('drawImage', "Show site PhenoCam Image", value = TRUE),
-                                      checkboxInput("drawImageROI", "Show ROI on PhenoCam Image", value = FALSE),
-                                      selectInput('pftSelection', 'ROI PFT selection', c('DB', 'EN', 'MX')),
-=======
                                       checkboxInput('drawImage', "Show site phenocamImage", value = TRUE),
                                       checkboxInput("drawImageROI", "Show roi on phenocamImage", value = FALSE),
                                       selectInput('pftSelection', 'Roi pft selection', ''),
->>>>>>> 3ee04106723275512340443ff75859de88e7be46
                                       actionButton('showModisSubset', 'Plot MODIS subset'),
                                       actionButton('plotPhenocamGCC', 'Plot GCC')
                                       ),
 
                         absolutePanel(id = 'currentImage', class = 'panel panel-default', #fixed = TRUE,
-                                      draggable = TRUE,  top = 'auto', left = 20, right = 'auto' , bottom = 20, 
+                                      draggable = TRUE,  top = 'auto', left = 20, right = 'auto' , bottom = 20,
                                       width = 375, height = 225,
                                       tags$div(id = 'image')
                                       ),
-                        
+
                         absolutePanel(id = 'plotpanel', class = 'panel panel-default', #fixed = TRUE,
-                                      draggable = TRUE,  top = 'auto', left = 20, right = 'auto' , bottom = 270, 
+                                      draggable = TRUE,  top = 'auto', left = 20, right = 'auto' , bottom = 270,
                                       width = 375, height = 225,
                                       plotOutput("currentPlot", height = 225)
                                       ),
-                        
+
                         absolutePanel(id = 'mouseBox', class = 'panel panel-default', fixed = TRUE,
-                                      draggable = FALSE,  top = 'auto', left = 'auto', right = 20 , bottom = 20, 
+                                      draggable = FALSE,  top = 'auto', left = 'auto', right = 20 , bottom = 20,
                                       width = 240, height = 40,
                                       verbatimTextOutput("mouse")
                                       ),
@@ -65,7 +59,7 @@ ui = fluidPage(shinyjs::useShinyjs(), navbarPage("PhenoSynth-development phase",
                                       draggable = FALSE, top = 150, left = 'auto', right = 320, bottom = 'auto',
                                       actionButton('hidePlot', 'Hide Plot')
                                       ),
-                        
+
                         absolutePanel(id = 'siteTitle', class = 'panel panel-default', fixed = FALSE,
                                       draggable = FALSE,  top = 25, left = 'auto', right = 320 , bottom = 'auto',
                                       div(id = 'analyzerHeader',uiOutput("analyzerTitle"), style = 'font-size: 40px; font-weight: 400;')
@@ -77,28 +71,28 @@ ui = fluidPage(shinyjs::useShinyjs(), navbarPage("PhenoSynth-development phase",
                             ) # close div outer
                         ), # close tab panel
 
-           
+
            tabPanel('pAOI Management',
-                    
+
                     tags$div(id='pAOItab'),
                     selectInput('shapefiles', "Select Shapefile", c('None')),
                     actionButton('saveshp', 'Save Shapefile'),
                     br(),
                     br(), br(),
 
-                    
+
                     # Attempting to build a chart here for the shapefiles, mihgt move it to a new tab at
                     #   some point......
                     DTOutput("pAOIchart")
 
                    ),
-           
-           
+
+
            tabPanel('Phenocam Table',
                     DTOutput('x1')
                    ),
-           
-           
+
+
            conditionalPanel("false", icon("crosshair"))
   )
 )
