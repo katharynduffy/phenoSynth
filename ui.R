16f0096013166f9d2e3e6f07ef243938531a0c06
katharynduffy <kdw223@nau.edu>
2018-08-08 08:59:35 -0700

    Merge branch 'master' of https://github.com/katharynduffy/phenoRemote

--- ui.R
+++ ui.R
@@ -94,7 +94,7 @@ ui = fluidPage(shinyjs::useShinyjs(), navbarPage("PhenoSynth-development phase",
 
                    ),
            tabPanel('User Guide',
-                    includeMarkdown('readme.rmd')
+                    includeMarkdown('UserGuide.md')
                     ),
 
 
