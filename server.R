91755572b377cdbbaa2b8e436e1b02fcb19273d5
Kyle E <kenns@usgs.gov>
2018-09-18 09:52:01 -0600

    changing last element to correct Vegetation Cover Agreement group in leaflet

--- server.R
+++ server.R
@@ -581,13 +581,14 @@ server = function(input, output, session) {
           clearControls() %>%
           clearImages() %>%
           addRasterImage(data$r, opacity = .65, project=TRUE, group='MODIS Land Cover 2016', colors = c3$colors) %>%
-          addRasterImage(rc, opacity = .55, project=TRUE, group= 'MODIS Reclassified 2016', colors= c('green','black')) %>%
-          addLegend(labels = c3$names, colors = c3$colors, position = "bottomleft", opacity = .95) %>%
+          addRasterImage(rc, opacity = .55, project=TRUE, group= 'Vegetation Cover Agreement', colors= c('green','black')) %>%
+          addLegend(labels = c3$names, group = 'MODIS Land Cover 2016', colors = c3$colors, position = "bottomleft", opacity = .95) %>%
           addLayersControl(baseGroups = c("World Imagery", "Open Topo Map"),
                            overlayGroups = c('MODIS Land Cover 2016', 'Vegetation Cover Agreement'),
                            position = c("topleft"),
                            options = layersControlOptions(collapsed = FALSE)) %>%
-          hideGroup('MODIS Reclassified 2016')
+          hideGroup('MODIS Land Cover 2016') %>%
+          clearControls()
     }
   })
 
