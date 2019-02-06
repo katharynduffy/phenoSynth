467e6c6b2899b2a2501ba722d8e1c3b6a08a6fb0
Kyle E <kenns@usgs.gov>
2019-02-06 14:19:29 -0700

    added plotly to gcc p=plot

--- server.R
+++ server.R
@@ -825,12 +825,6 @@ server = function(input, output, session) {
     #   p = ggplot(df) + geom_point() + xlim(0, 10) + ylim(0, 1) + ggtitle('Select a Pixel!!')
     # }
 
-    
-    # Plot p
-    output$ndvi_pixels_plot = renderPlot({
-      p
-    })
-
     pp = ggplotly(p) %>% config(displaylogo = FALSE,
                                 modeBarButtonsToRemove = list(
                                   'sendDataToCloud',
@@ -841,8 +835,147 @@ server = function(input, output, session) {
                                   'toggleSpikelines'
                                 ))
 
+    # output$data_plot = renderPlotly({
+    #   pp
+    # })
+    
+    
+    df_test = phenocam$gcc
+    spring  = phenocam$spring
+    fall    = phenocam$fall
+    print (head(df_test))
+    
+    unix = "1970-01-01"
+    spring[, 2:9] = apply(spring[, 2:9], 2, function(x)
+      as.character(as.Date(x, origin = unix)))
+    fall[, 2:9] = apply(fall[, 2:9], 2, function(x)
+      as.character(as.Date(x, origin = unix)))
+
+    p = plot_ly(
+      data = df_test,
+      x = ~ date,
+      y = ~ gcc_90,
+      showlegend = FALSE,
+      type = 'scatter',
+      mode = 'markers'
+    ) %>%
+      add_trace(
+        y = ~ smooth_gcc_90,
+        mode = "lines",
+        line = list(width = 2, color = "rgb(120,120,120)"),
+        name = "Gcc loess fit",
+        showlegend = TRUE
+      ) %>%
+      # SOS spring
+      # 10%
+      add_trace(
+        data = spring,
+        x = ~ as.Date(transition_10),
+        y = ~ threshold_10,
+        mode = "markers",
+        type = "scatter",
+        marker = list(color = "#7FFF00", symbol = "circle"),
+        name = "SOS (10%)",
+        showlegend = TRUE
+      ) %>%
+      add_segments(x = ~ as.Date(transition_10_lower_ci),
+                   xend = ~ as.Date(transition_10_upper_ci),
+                   y = ~ threshold_10,
+                   yend = ~ threshold_10,
+                   line = list(color = "#7FFF00"),
+                   name = "SOS (10%) - CI"
+      ) %>%
+      # 25 %
+      add_trace(
+        x = ~ as.Date(transition_25),
+        y = ~ threshold_25,
+        mode = "markers",
+        type = "scatter",
+        marker = list(color = "#66CD00", symbol = "square"),
+        showlegend = TRUE,
+        name = "SOS (25%)"
+      ) %>%
+      add_segments(x = ~ as.Date(transition_25_lower_ci),
+                   xend = ~ as.Date(transition_25_upper_ci),
+                   y = ~ threshold_25,
+                   yend = ~ threshold_25,
+                   line = list(color = "#66CD00"),
+                   name = "SOS (25%) - CI"
+      ) %>%
+      # 50 %
+      add_trace(
+        x = ~ as.Date(transition_50),
+        y = ~ threshold_50,
+        mode = "markers",
+        type = "scatter",
+        marker = list(color = "#458B00", symbol = "diamond"),
+        showlegend = TRUE,
+        name = "SOS (50%)"
+      ) %>%
+      add_segments(x = ~ as.Date(transition_50_lower_ci),
+                   xend = ~ as.Date(transition_50_upper_ci),
+                   y = ~ threshold_50,
+                   yend = ~ threshold_50,
+                   line = list(color = "#458B00"),
+                   name = "SOS (50%) - CI"
+      ) %>%
+      # EOS fall
+      # 50%
+      add_trace(
+        data = fall,
+        x = ~ as.Date(transition_50),
+        y = ~ threshold_50,
+        mode = "markers",
+        type = "scatter",
+        marker = list(color = "#FFB90F", symbol = "diamond"),
+        showlegend = TRUE,
+        name = "EOS (50%)"
+      ) %>%
+      add_segments(x = ~ as.Date(transition_50_lower_ci),
+                   xend = ~ as.Date(transition_50_upper_ci),
+                   y = ~ threshold_50,
+                   yend = ~ threshold_50,
+                   line = list(color = "#FFB90F"),
+                   name = "EOS (50%) - CI"
+      ) %>%
+      # 25 %
+      add_trace(
+        x = ~ as.Date(transition_25),
+        y = ~ threshold_25,
+        mode = "markers",
+        type = "scatter",
+        marker = list(color = "#CD950C", symbol = "square"),
+        showlegend = TRUE,
+        name = "EOS (25%)"
+      ) %>%
+      add_segments(x = ~ as.Date(transition_25_lower_ci),
+                   xend = ~ as.Date(transition_25_upper_ci),
+                   y = ~ threshold_25,
+                   yend = ~ threshold_25,
+                   line = list(color = "#CD950C"),
+                   name = "EOS (25%) - CI"
+      ) %>%
+      # 10 %
+      add_trace(
+        x = ~ as.Date(transition_10),
+        y = ~ threshold_10,
+        mode = "markers",
+        marker = list(color = "#8B6508", symbol = "circle"),
+        showlegend = TRUE,
+        name = "EOS (10%)"
+      ) %>%
+      add_segments(x = ~ as.Date(transition_10_lower_ci),
+                   xend = ~ as.Date(transition_10_upper_ci),
+                   y = ~ threshold_10,
+                   yend = ~ threshold_10,
+                   line = list(color = "#8B6508"),
+                   name = "EOS (10%) - CI"
+      ) %>%
+      layout(xaxis = list(title = "Date"),
+             yaxis = list(title = "Gcc"))
+    
     output$data_plot = renderPlotly({
-      pp
+      p
     })
 
     output$event_plot = renderPrint({
@@ -1104,7 +1237,9 @@ server = function(input, output, session) {
       fall_filepath   = paste0(file_path, 'gcc', '_',paste0(freq,'_day_fall_tds'), '.csv')
       if (input$localDownload){
         if (file.exists(gcc_filepath)){
-          phenocam$gcc = read.csv(gcc_filepath, header = TRUE)
+          phenocam$gcc    = read.csv(gcc_filepath, header = TRUE)
+          phenocam$spring = read.csv(spring_filepath, header = TRUE)
+          phenocam$fall   = read.csv(fall_filepath, header = TRUE)
         }else{
           phenocam$data = get_site_roi_csvs(name        = site,
                                             roi_files_  = roi_files,
@@ -1113,12 +1248,8 @@ server = function(input, output, session) {
                                             roi_type_   = pft_abb)
 
           phenocam$gcc    = phenocam$data[[1]]
-          phenocam$spring    = phenocam$data[[2]]
-          phenocam$fall    = phenocam$data[[3]]
-          # phenocam$spring = apply(phenocam$data[[2]][, 2:9], 2, function(x)
-          #   as.character(as.Date(x, origin = unix)))
-          # phenocam$fall = apply(phenocam$data[[3]][, 2:9], 2, function(x)
-          #   as.character(as.Date(x, origin = unix)))
+          phenocam$spring = phenocam$data[[2]]
+          phenocam$fall   = phenocam$data[[3]]
           
           write.csv(phenocam$gcc,    file = gcc_filepath)
           write.csv(phenocam$spring, file = spring_filepath)
@@ -1126,7 +1257,9 @@ server = function(input, output, session) {
         }
       } else{
         if (file.exists(gcc_filepath)){
-          phenocam$gcc = read.csv(gcc_filepath, header = TRUE)
+          phenocam$gcc    = read.csv(gcc_filepath, header = TRUE)
+          phenocam$spring = read.csv(spring_filepath, header = TRUE)
+          phenocam$fall   = read.csv(fall_filepath, header = TRUE)
         } else{
           phenocam$data = get_site_roi_csvs(name        = site,
                                             roi_files_  = roi_files,
@@ -1137,10 +1270,6 @@ server = function(input, output, session) {
           phenocam$gcc    = phenocam$data[[1]]
           phenocam$spring    = phenocam$data[[2]]
           phenocam$fall    = phenocam$data[[3]]
-          # phenocam$spring = apply(phenocam$data[[2]][, 2:9], 2, function(x)
-          #   as.character(as.Date(x, origin = unix)))
-          # phenocam$fall = apply(phenocam$data[[3]][, 2:9], 2, function(x)
-          #   as.character(as.Date(x, origin = unix)))
         }
       }
       
