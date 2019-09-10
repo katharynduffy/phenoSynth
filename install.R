ec5e03e4ca5f986cdd357d3b7cc32dd2c9ffdbb9
christian71400 <christianjimenez71400@gmail.com>
2019-09-10 15:02:49 -0700

    Got the for loop running

--- install.R
+++ install.R
@@ -1,4 +1,8 @@
 #packages you will need for this shiny app
-for (package in c('shiny','shinyjs','leaflet','leaflet.extras','httr','tidyr','readr','scales','lattice','jsonlite', 'DT', 'shinyBS', 'leaflet.extras', 'sp', 'ncdf4', 'rvest', 'ggthemes', 'httr', 'shinycssloaders','data.table', 'grDevices', 'plotly' , 'knitr', 'kableExtra')) {
-  install.packages(package)  
+packages = c('shiny','shinyjs','leaflet','leaflet.extras','httr','tidyr','readr','scales','lattice','jsonlite', 'DT', 'shinyBS', 'leaflet.extras', 'sp', 'ncdf4', 'rvest', 'ggthemes', 'httr', 'shinycssloaders','data.table', 'grDevices', 'plotly' , 'knitr', 'kableExtra', 'rgdal', 'rjson')
+for (i in 1:length(packages))
+{
+  install.packages(packages[i])
 }
+
+
