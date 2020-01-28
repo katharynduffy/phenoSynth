8aa2619dcc7bc37118d7299ddaef41ee4c487954
Enns, Kyle Douglas <kenns@usgs.gov>
2020-01-28 09:21:48 -0700

    adding Landcover nlcd for new phenocam sites

--- global.R
+++ global.R
@@ -113,3 +113,4 @@ if (file.exists('./config.R')){
 sinu_crs = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
 merc_crs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
 wgs_crs  = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+alb_crs  = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
